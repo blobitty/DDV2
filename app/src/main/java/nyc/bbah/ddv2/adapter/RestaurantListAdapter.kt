acc2ba7171c7b399aacfa460d21966384621089c
Bouabacar <boubacarbah@ac.c4q.nyc>
2019-02-20 13:45:18 -0500

    impl Vholder & Adapter

---
+++ app/src/main/java/nyc/bbah/ddv2/adapter/RestaurantListAdapter.kt
@@ -0,0 +1,29 @@
+package nyc.bbah.ddv2.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import nyc.bbah.ddv2.model.Restaurant
+import nyc.bbah.ddv2.viewmodel.RestaurantListViewHolder
+import nyc.bbah.ddv2.R.layout
+
+class RestaurantListAdapter(val items: List<Restaurant>) : RecyclerView.Adapter<RestaurantListViewHolder>() {
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RestaurantListViewHolder {
+        return RestaurantListViewHolder(LayoutInflater
+            .from(p0.context)
+            .inflate(layout.discoverlist_itemview, p0, false))
+    }
+
+    override fun onBindViewHolder(p0: RestaurantListViewHolder, p1: Int) {
+        val restaurant: Restaurant = items[p1]
+        p0.onBind(restaurant)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+}
--- app/src/main/java/nyc/bbah/ddv2/viewmodel/RestaurantListViewHolder.kt
+++ app/src/main/java/nyc/bbah/ddv2/viewmodel/RestaurantListViewHolder.kt
@@ -2,9 +2,24 @@ package nyc.bbah.ddv2.viewmodel
 
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.View
-import nyc.bbah.ddv2.MainContract
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.discoverlist_itemview.view.*
+import nyc.bbah.ddv2.model.Restaurant
 
 class RestaurantListViewHolder(itemView: View?): ViewHolder(itemView!!) {
 
 
+    fun onBind(restaurant: Restaurant){
+
+        itemView.restaurantname?.text = restaurant.name
+        itemView.cuisine_tv?.text = restaurant.description
+        itemView.asaptime_tv?.text = restaurant.status
+
+        val imgUrl: String? = restaurant.cover_img_url
+
+        Picasso
+            .get()
+            .load(imgUrl)
+            .into(itemView.restaurantlist_imageView)
+    }
 }
