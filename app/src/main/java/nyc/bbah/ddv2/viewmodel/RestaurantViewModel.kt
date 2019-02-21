99decafc0aa7bdd4b81c9873a49538bc9317ba94
Bouabacar <boubacarbah@ac.c4q.nyc>
2019-02-21 10:56:34 -0500

    added viewmodel

--- app/src/main/java/nyc/bbah/ddv2/viewmodel/RestaurantViewModel.kt
+++ app/src/main/java/nyc/bbah/ddv2/viewmodel/RestaurantViewModel.kt
@@ -1,2 +1,25 @@
 package nyc.bbah.ddv2.viewmodel
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import nyc.bbah.ddv2.model.Restaurant
+
+class RestaurantViewModel : ViewModel(){
+
+
+    private val restaurants: MutableLiveData<List<Restaurant>> by lazy {
+        MutableLiveData<List<Restaurant>>().also {
+            loadRestaurants()
+        }
+    }
+
+    fun getRestaurants(): LiveData<List<Restaurant>> {
+        return restaurants
+    }
+
+    private fun loadRestaurants() {
+        // Do an asynchronous operation to fetch restaurants.
+    }
+
+}
