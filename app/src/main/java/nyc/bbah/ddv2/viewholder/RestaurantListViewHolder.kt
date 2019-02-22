cdbf2ea114dadc87e7abd91c21711a041bf7cb73
Bouabacar <boubacarbah@ac.c4q.nyc>
2019-02-22 11:31:05 -0500

    done with UI fix

--- app/src/main/java/nyc/bbah/ddv2/viewholder/RestaurantListViewHolder.kt
+++ app/src/main/java/nyc/bbah/ddv2/viewholder/RestaurantListViewHolder.kt
@@ -6,19 +6,42 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.discoverlist_itemview.view.*
 import nyc.bbah.ddv2.RestaurantsNavigator
 import nyc.bbah.ddv2.model.Restaurant
+import java.lang.StringBuilder
 
 class RestaurantListViewHolder(itemView: View?, private val navigator: RestaurantsNavigator): ViewHolder(itemView!!) {
 
 
     fun onBind(restaurant: Restaurant){
+        val maxLength = 20
+        val shortDesc = StringBuilder()
+        //val shortName = StringBuilder()
 
 
-        itemView.restaurantname?.text = restaurant.name
-        itemView.cuisine_tv?.text = restaurant.description
-        itemView.asaptime_tv?.text = restaurant.status
+        //shorten cuisine string logic
+        if(restaurant.description!!.length > maxLength){
+            shortDesc.append(restaurant.description.substring(0, maxLength))
+            shortDesc.append("...")
+            itemView.cuisine_tv?.text = shortDesc.toString()
+        } else{
+            itemView.cuisine_tv?.text = restaurant.description
+        }
+
+        //shorten restaurant name logic
+        if (restaurant.name!!.contains("(")){
+            val index = restaurant.name.indexOf('(')
+            val shortName: String? = restaurant.name.substring(0, index)
+            itemView.restaurantname?.text = shortName
+        } else{
+            itemView.restaurantname?.text = restaurant.name
+        }
+        //check if status has time
+        if(restaurant.status!!.contains("Pre")){
+            itemView.asaptime_tv?.text = ""
+        } else{
+            itemView.asaptime_tv?.text = restaurant.status
+        }
 
         val imgUrl: String? = restaurant.cover_img_url
-
         Picasso
             .get()
             .load(imgUrl)
